433338c755274859d5238c540489cc057f429020
Aubry Mangold <aubry.mangold@heig-vd.ch>
2022-12-26 14:23:46 +0100

    Make allocation more robust and add matrix creation utils

--- inc/matrix.h
+++ inc/matrix.h
@@ -11,15 +11,30 @@ typedef struct Matrix {
     double** data;
 } Matrix;
 
+// Constructors.
 Matrix* mat_create(uint row_cnt, uint col_cnt, ...);
 Matrix* mat_alloc(uint row_cnt, uint col_cnt);
 void    mat_free(Matrix* mat);
+
+// Copying.
+Matrix* mat_copy(const Matrix* mat);
+
+// Creation
+Matrix* mat_square(uint size, ...);
+Matrix* mat_identity(uint size);
 
 // Operations.
 Matrix* mat_transpose(const Matrix* mat);
 Matrix* mat_add(const Matrix* m1, const Matrix* m2);
 Matrix* mat_sub(const Matrix* m1, const Matrix* m2);
 Matrix* mat_multiply(const Matrix* m1, const Matrix* m2);
+Matrix* mat_divide(const Matrix* m1, const Matrix* m2);
+
+// Scalar operations.
+// Matrix* mat_scalar_add(const Matrix* mat, double scalar);
+// Matrix* mat_scalar_sub(const Matrix* mat, double scalar);
+// Matrix* mat_scalar_multiply(const Matrix* mat, double scalar);
+// Matrix* mat_scalar_divide(const Matrix* mat, double scalar);
 
 // 0-based indexing.
 Matrix* mat_get_row(const Matrix* mat, uint row_idx);
--- src/matrix.c
+++ src/matrix.c
@@ -1,10 +1,14 @@
 #include <matrix.h>
 #include <ops.h>
+#include <io.h>
 
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
+#define MATRIX_ERR_ALLOC_MSG "Error while allocating memory for matrix."
+#define MATRIX_ERR_DATA_ALLOC_MSG "Error while allocating memory for matrix data."
 
 Matrix* mat_create(uint row_cnt, uint col_cnt, ...) {
     Matrix* mat = mat_alloc(row_cnt, col_cnt);
@@ -25,11 +29,26 @@ Matrix* mat_create(uint row_cnt, uint col_cnt, ...) {
 
 Matrix* mat_alloc(uint row_cnt, uint col_cnt) {
     Matrix* mat = malloc(sizeof(Matrix));
+    if (mat == NULL) {
+        log_err(MATRIX_ERR_ALLOC_MSG);
+        exit(EXIT_FAILURE);
+    }
+
     mat->row_cnt = row_cnt;
     mat->col_cnt = col_cnt;
+
     mat->data = (double**)calloc(mat->row_cnt, sizeof(double*));
+    if (mat->data == NULL) {
+        log_err(MATRIX_ERR_DATA_ALLOC_MSG);
+        exit(EXIT_FAILURE);
+    }
+
     for (size_t i = 0; i < mat->row_cnt; ++i) {
         mat->data[i] = (double*)calloc(mat->col_cnt, sizeof(double));
+        if (mat->data[i] == NULL) {
+            log_err(MATRIX_ERR_DATA_ALLOC_MSG);
+            exit(EXIT_FAILURE);
+        }
     }
 
     return mat;
@@ -43,6 +62,32 @@ void mat_free(Matrix* mat) {
     free(mat);
 }
 
+// Copying.
+Matrix* mat_copy(const Matrix* mat) {
+    Matrix* mat_copy = mat_alloc(mat->row_cnt, mat->col_cnt);
+    memcpy(mat_copy->data, mat->data, mat->row_cnt * mat->col_cnt * sizeof(double));
+    return mat_copy;
+}
+
+// Creation
+Matrix* mat_square(uint size, ...) {
+    Matrix* mat = mat_alloc(size, size);
+
+    va_list args;
+    va_start(args, size);
+
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t j = 0; j < size; ++j) {
+            mat->data[i][j] = va_arg(args, double);
+        }
+    }
+
+    va_end(args);
+
+    return mat;
+}
+
+Matrix* mat_identity(uint size);
 
 Matrix* mat_transpose(const Matrix* mat) {
     Matrix* mat_t = mat_alloc(mat->col_cnt, mat->row_cnt);
@@ -65,8 +110,8 @@ Matrix* mat_add(const Matrix* m1, const Matrix* m2) {
 
     Matrix* mat = mat_create(m1->row_cnt, m1->col_cnt);
 
-    for(size_t i = 0; i < m1->row_cnt; ++i) {
-        for(size_t j = 0; j < m1->col_cnt; ++j) {
+    for (size_t i = 0; i < m1->row_cnt; ++i) {
+        for (size_t j = 0; j < m1->col_cnt; ++j) {
             mat->data[i][j] = m1->data[i][j] + m2->data[i][j];
         }
     }
@@ -83,8 +128,8 @@ Matrix* mat_sub(const Matrix* m1, const Matrix* m2) {
 
     Matrix* mat = mat_create(m1->row_cnt, m1->col_cnt);
 
-    for(size_t i = 0; i < m1->row_cnt; ++i) {
-        for(size_t j = 0; j < m1->col_cnt; ++j) {
+    for (size_t i = 0; i < m1->row_cnt; ++i) {
+        for (size_t j = 0; j < m1->col_cnt; ++j) {
             mat->data[i][j] = m1->data[i][j] - m2->data[i][j];
         }
     }
@@ -101,7 +146,7 @@ Matrix* mat_multiply(const Matrix* m1, const Matrix* m2) {
 
     Matrix* m2_t = mat_transpose(m2);
     Matrix* res = mat_create(m1->row_cnt, m2->col_cnt);
-    
+
     // Multiply matrices.
     for (size_t i = 0; i < m1->row_cnt; ++i) {
         for (size_t j = 0; j < m2->col_cnt; ++j) {
@@ -110,8 +155,26 @@ Matrix* mat_multiply(const Matrix* m1, const Matrix* m2) {
     }
 
     free(m2_t);
-    
+
     return res;
+}
+
+Matrix* mat_divide(const Matrix* m1, const Matrix* m2) {
+    // Check if matrices are compatible.
+    if (m1->row_cnt != m2->row_cnt || m1->col_cnt != m2->col_cnt) {
+        fprintf(stderr, "Error: matrices are incompatible\nExiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Matrix* mat = mat_create(m1->row_cnt, m1->col_cnt);
+
+    for (size_t i = 0; i < m1->row_cnt; ++i) {
+        for (size_t j = 0; j < m1->col_cnt; ++j) {
+            mat->data[i][j] = m1->data[i][j] / m2->data[i][j];
+        }
+    }
+
+    return mat;
 }
 
 Matrix* mat_get_row(const Matrix* mat, uint row_idx) {
